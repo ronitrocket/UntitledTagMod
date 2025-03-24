c3ddc7bfb3a4b81eb307baf9dedfbff472718c8c
ronitrocket <ronitrocket98@gmail.com>
2025-03-24 18:00:59 -0400

    added localization file for english

---
+++ localization/en-us.lua
@@ -0,0 +1,29 @@
+return {
+    description ={
+        Enhanced = {
+            m_untitledtagmod_taggerOld = {
+                name = "Tagger",
+                text = {
+                    "{C:mult}+#1#{} Mult",
+                    "Tags the card to",
+                    "the right"
+                },
+            },
+        },
+        Other = {
+            untitledtagmod_tagger = {
+                name = "Tagger",
+                text = {
+                    "{C:mult}+#1#{} Mult",
+                    "Tags the card to",
+                    "the right"
+                },
+            },
+        },
+    },
+    misc = {
+        labels = {
+            untitledtagmod_tagger = "Tagger",
+        },
+    },
+}
